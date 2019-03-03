9b5c35a750ad9038a4234aed5ac528e943d83049
leafiy <t@leafiy.com>
2019-03-04 00:32:11 +0800

    update random code

--- string/randomCode.js
+++ string/randomCode.js
@@ -1,10 +1,9 @@
-const genCode = function (len = 6) {
-  let code = ''
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  for (let i = 0; i < len; i++) {
-    code += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
-  return code
+const genCode = function(len = 6, content = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
+    let code = ''
+    for (let i = 0; i < len; i++) {
+        code += content.charAt(Math.floor(Math.random() * content.length))
+    }
+    return code
 }
 
 export default genCode
