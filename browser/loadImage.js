0363968cc491451d737e0e57e91d56c825a8e608
leafiy <t@leafiy.com>
2019-03-14 00:55:03 +0800

    update image load

--- browser/loadImage.js
+++ browser/loadImage.js
@@ -2,10 +2,9 @@ const loadImage = function (url) {
   return new Promise((resolve, reject) => {
     let img = new Image()
     img.onload = function () {
-      resolve(url)
+      resolve(url,img)
     }
     img.onerror = function () {
-      console.log(url)
       reject(url)
     }
     img.src = url
