1d294048fc179fdfc8c84b18d86a33fca3574fac
leafiy <t@leafiy.com>
2019-04-17 14:34:42 +0800

    add memoize

---
+++ utils/memoize.js
@@ -0,0 +1,15 @@
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    } else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+export default memoize
