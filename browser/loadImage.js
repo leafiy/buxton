e5a20805f975fe31bd2f1d94bbfda5ee26f0de96
leafiy <t@leafiy.com>
2019-04-25 02:42:44 +0800

    add image load timeout

--- browser/loadImage.js
+++ browser/loadImage.js
@@ -1,34 +1,45 @@
 import race from '../promise/race.js'
 const loadImage = function(src, timeout) {
   return new Promise((resolve, reject) => {
+
+    let timer;
     let img = new Image()
-    if (timeout) {
-      race(timeout, new Promise((resolve, reject) => {
-        img.onload = function() {
-          resolve({
-            src: src,
-            width: img.naturalWidth,
-            height: img.naturalHeight
-          })
-        }
-        img.onerror = function() {
-          reject(src)
-        }
-      }))
-    } else {
-      img.onload = function() {
-        resolve({
-          src: src,
-          width: img.naturalWidth,
-          height: img.naturalHeight
-        })
-      }
-      img.onerror = function() {
-        reject(src)
+
+    function clearTimer() {
+      if (timer) {
+        clearTimeout(timer)
+        timer = null
       }
     }
 
+    function handleFailed(err) {
+      img.onload = img.onabort = img.onerror = function() {};
+      clearTimeout(timer)
+      reject(err instanceof Error ? err : new Error('failed'))
+    }
+
+
+    img.onerror = img.onabort = handleFailed;
+
+
+    img.onload = function() {
+      clearTimer();
+      resolve({
+        src: src,
+        width: img.naturalWidth,
+        height: img.naturalHeight
+      })
+
+    };
     img.src = src
+    if (timeout) {
+      timer = setTimeout(() => {
+        handleFailed(new Error('timeout'))
+      }, timeout);
+    }
+
+
+
   })
 }
 
--- promise/race.js
+++ promise/race.js
@@ -1,40 +1,10 @@
-const race = function(timeout, promise) {
-  return new Promise(function(resolve, reject) {
-    let timer = setTimeout(function() {
-      reject(new Error("timeout"));
-    }, timeout);
-
-    promise
-      .then(function(res) {
-        clearTimeout(timer);
-        resolve(res);
-      })
-      .catch(function(err) {
-        clearTimeout(timer);
-        reject(err);
-      });
-  });
+const race = function(timeout, cb) {
+  return Promise.race([
+    new Promise(cb),
+    new Promise(function(resolve, reject) {
+      setTimeout(function() { reject('Timed out'); }, timeout);
+    })
+  ]);
 }
 
 export default race
-
-
-// usage
-
-// function delayedHello(cb) {
-//   setTimeout(function() {
-//     cb('Hello');
-//   }, 1000);
-// }
-
-// Promise.timeout(800, delayedHello).then(function(data) {
-//   console.log(data);
-// }).catch(function(e) {
-//   console.log(e);
-// });
-
-// Promise.timeout(1200, delayedHello).then(function(data) {
-//   console.log(data);
-// }).catch(function(e) {
-//   console.log(e);
-// });
